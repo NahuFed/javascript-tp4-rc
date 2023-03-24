cd3a9d83ed81eb9429485465b730ae1d32e230fe
Nahuel Rodriguez <nahufed@gmail.com>
2023-03-24 17:00:18 -0300

    modificados precios para que esten acorde a la realidad

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -5,13 +5,13 @@ class Producto{
         this.precio=precio
     }
     get imprimeDatos(){
-        return document.write(`codigo: ${this.codigo} <br>nombre:${this.nombre}<br>precio:${this.precio}<hr>`)
+        return document.write(`codigo: ${this.codigo} <br>nombre: ${this.nombre}<br>precio: $${this.precio}<hr>`)
     }
 }
 
-let producto1 = new Producto(1,'azucar',120)
-let producto2 = new Producto(3,'leche',220)
-let producto3 = new Producto(3,'cafe',190)
+let producto1 = new Producto(1,'Azucar',320)
+let producto2 = new Producto(3,'Leche',217)
+let producto3 = new Producto(3,'Cafe',839)
 
 let arrayProductos = [producto1,producto2,producto3]
 
