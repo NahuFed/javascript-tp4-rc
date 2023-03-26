c099a0048aa7879158892359a78c16888593ea94
Nahuel Rodriguez <nahufed@gmail.com>
2023-03-26 20:02:26 -0300

    agregado ejercicio 6

---
+++ js/ejercicio6.js
@@ -0,0 +1,61 @@
+const determinarMayorNumeroDePaginas = (libro1,libro2) =>{
+  return libro1.obtenerNumeroDePaginas >= libro2.obtenerNumeroDePaginas? `El libro ${libro1.obtenerTitulo} tiene mas paginas`: `El libro ${libro2.obtenerTitulo} tiene mas paginas`
+}
+
+class Libro {
+  constructor(isbn, titulo, autor, numeroDePaginas) {
+    this._isbn = isbn;
+    this._titulo = titulo;
+    this._autor = autor;
+    this._numeroDePaginas = numeroDePaginas;
+  }
+
+  set modificarIsbn(isbn) {
+    this._isbn = isbn;
+  }
+  get obtenerIsbn() {
+    return this._isbn;
+  }
+  set modificarTitulo(titulo) {
+    this._titulo = titulo;
+  }
+  get obtenerTitulo() {
+    return this._titulo;
+  }
+  set modificarAutor(autor) {
+    this._autor = autor;
+  }
+  get obtenerAutor() {
+    return this._autor;
+  }
+  set modificarNumeroDePaginas(numeroDePaginas) {
+    this._numeroDePaginas = numeroDePaginas;
+  }
+  get obtenerNumeroDePaginas() {
+    return this._numeroDePaginas;
+  }
+
+  mostrarLibro() {
+    return document.write(
+      `El libro ${this._titulo} con ISBN ${this._isbn} creado por el autor ${this._autor} tiene ${this._numeroDePaginas} páginas<hr>`
+    );
+  }
+}
+
+let elPrincipito = new Libro(
+  9783140464079,
+  "El Principito",
+  "Antoine de Saint-Exupéry",
+  120
+);
+let harryPotter = new Libro(
+  9788498381405,
+  "Harry Potter y las reliquias de la muerte",
+  "J.K. Rowling",
+  640
+);
+
+harryPotter.mostrarLibro()
+elPrincipito.mostrarLibro()
+
+console.log(determinarMayorNumeroDePaginas(elPrincipito,harryPotter))
